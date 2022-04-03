fb8ce0cf6db35a78e1f65d097c50a25a5fa14271
Arun Shekhar <arunshekhar001@gmail.com>
2022-04-03 21:09:47 +0530

    footer fix

--- packages/react-app/src/components/shared/Footer/index.js
+++ packages/react-app/src/components/shared/Footer/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 import { AiFillHeart } from "react-icons/ai";
-import'./style.css';
-export const LandingFooter = (props)=>{
-    return (
-      <div className="w-screen bg-white/[0.25] dark:bg-black/[0.25] filter backdrop-blur-sm">
-        <div className="flex justify-center items-center text-[18px] py-[7px] text-gray-800 dark:text-gray-400">
-          © {new Date().getFullYear()}, Made with &nbsp;
-          <AiFillHeart className="Heart text-red-700" />
-          &nbsp; Love and Care
-        </div>
+import { FaFileMedicalAlt } from "react-icons/fa";
+import "./style.css";
+export const LandingFooter = (props) => {
+  return (
+    <div className="w-screen bg-white/[0.25] dark:bg-black/[0.25] filter backdrop-blur-sm">
+      <div className="flex justify-center items-center text-[#0ac5a8] text-[20px] font-bold pt-[7px]">
+        <FaFileMedicalAlt
+          className="text-[20px] mr-2 text-gray-800 dark:text-white"
+          aria-hidden="true"
+        />
+        Docthereum
       </div>
-    );
+      <div className="flex justify-center items-center text-[18px] py-[7px] text-gray-800 dark:text-gray-400">
+        © {new Date().getFullYear()}, Made with &nbsp;
+        <AiFillHeart className="Heart text-red-700" />
+        &nbsp; Love and Care
+      </div>
+    </div>
+  );
 };
 
 export const CustomFooter = (props) => {
@@ -18,8 +26,8 @@ export const CustomFooter = (props) => {
     <div className="fixed bottom-0 left-0 w-screen bg-white/[0.25] dark:bg-black/[0.25] filter backdrop-blur-sm">
       <div className="flex justify-center items-center text-[18px] py-[6px] text-gray-800 dark:text-gray-200">
         This page is only visible to&nbsp;
-        <span className="text-[#0ac5a8] font-bold">{props.user}</span>, you can access it
-        because it's visible for demonstration purposes.
+        <span className="text-[#0ac5a8] font-bold">{props.user}</span>, you can
+        access it because it's visible for demonstration purposes.
       </div>
     </div>
   );
